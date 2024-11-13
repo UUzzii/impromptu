5bac55c64c499c93cfa5927ff3c8a8b0bba08f7d
石鹏 <shipeng@shenzhoubb.com>
2024-11-13 09:23:45 +0800

    Hello World!

---
+++ impromptu-common/src/main/java/com/Main.java
@@ -0,0 +1,11 @@
+package com;
+
+/**
+ * @author 石鹏
+ * @date 2024/11/13 8:55
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
